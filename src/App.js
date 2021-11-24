872d55d2158d7f34fd4def9545e8830a8711510f
Cheryl <1030821728@qq.com>
2021-11-24 17:40:28 +0800

    ✨[功能更新]添加了路网匹配功能

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import "./App.css";
 import { Amap, PolylineEditor, Polyline } from "@amap/amap-react";
 import Papa from 'papaparse';
@@ -10,30 +10,31 @@ import {path2Array} from "@amap/amap-react/es/utils/convert";
 
 const {  Sider, Content } = Layout;
 
-console.log('Initial state: ', store.getState())
 
 // const unsubscribe = store.subscribe(() =>
 //   console.log('State after dispatch: ', store.getState().pointEditor[0])
 // )
+const URLType = {
+  "MATCH_ROUTE" : 0,
+  "CONVERT_POINT" : 1
+}
 export default function App() {
   const [pathArray, setpathArray] = useState([
     [
       120.585008,
       28.088507
     ],
-    // [
-    //   120.584518,
-    //   28.084903
-    // ],
-    // [
-    //   120.584518,
-    //   28.084903
-    // ],
-    // [
-    //   120.618367,
-    //   28.022355
-    // ]
   ]);
+  function locParser(position) {
+    let res = position.split(';');
+    res = res.map((x)=>{
+      let newPos = x.split(',');
+      newPos[0] = parseFloat(newPos[0])
+      newPos[1] = parseFloat(newPos[1])
+      return newPos;
+    });
+    return res;
+  }
   function sortArray() {
     let res = [];
     let temp = {};
@@ -48,15 +49,85 @@ export default function App() {
       }
       return item;
     })
-    console.log(res);
-    setpathArray(res[0]);
+    getWaypointsArray(res[0]);
+  }
+
+  function urlGenerator(type, start_i, Array){
+    let URL;
+    switch (type){
+      case URLType.CONVERT_POINT:
+        URL = "https://restapi.amap.com/v3/assistant/coordinate/convert?locations="
+        Array.slice(start_i, start_i + 40).map((item,idx)=>{
+          URL += item[0] + ',' + item[1];
+          if (idx !=  39){
+            URL +='|';
+          }
+        })
+        URL+="&coordsys=gps&key=7f8f5883aa922c1463b2710e97989156"
+        return URL;
+      case URLType.MATCH_ROUTE:
+        URL="http://router.project-osrm.org/match/v1/car/"
+        Array.slice(start_i, start_i + 100).map((item,idx)=>{
+          URL += item[0] + ',' + item[1];
+          if (idx != 99){
+            URL +=';';
+          }
+        })
+        URL += "?geometries=geojson&overview=full&tidy=true";
+        return URL;
+      default:
+        return '';
+    }
+  }
+  async function getWaypointsArray(pathArray){
+
+    var res = []
+    let i = 0, j = 0;
+    while (i + 100 < pathArray.length) {
+      let URL = urlGenerator(URLType.MATCH_ROUTE, i, pathArray);
+      await fetch(URL, {
+        method: 'GET',
+      })
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(myJson) {
+          myJson.matchings.map(x=>{
+            res = [...res, ...x.geometry.coordinates];
+          })
+        });
+      i += 100;
+    }
+    var pathAfterConvert = []
+    while(j + 40 < res.length){
+      let URL = urlGenerator(URLType.CONVERT_POINT, j, res);
+      await fetch(URL, {
+        method: 'GET',
+      })
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(myJson) {
+          pathAfterConvert = [...pathAfterConvert, locParser(myJson.locations)];
+        });
+      j += 40;
+    }
+    setpathArray(pathAfterConvert);
+  }
+  function getFormatLocationArray(waypoints){
+    let res = []
+    waypoints.map(item=>{
+      if (item && item.location)
+        res.push(item.location)
+    })
+    return res;
   }
   return (
     <div className="App">
       <Layout style={{height:'100%'}}>
         <Content>
           <div className="map-container">
-            <Amap zoom={15} center={[120.627, 27.9769]}>
+            <Amap zoom={15} center={[120.631, 27.9736]}>
               <PolylineEditor enabled={false} onChange={target => {setpathArray(path2Array(target.getPath()))}}>
                 <Polyline
                   path={pathArray}
@@ -79,12 +150,9 @@ export default function App() {
                   dynamicTyping: true,
                   step: function(row) {
                     store.dispatch(addcarPoint(row.data))
-                    // unsubscribe()
-                    // console.log("Row:", row.data);
                   },
-                  complete: function() {
-                    console.log('load Complete!');
-                    sortArray();
+                  complete: async function() {
+                    await sortArray();
                   }
                 });
               }}
